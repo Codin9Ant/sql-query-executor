6f3c12783fadaba8326da2e0b8926c7e5a4b5006
Codin9Ant <pmg1226@hanmail.net>
2022-12-20 01:40:42 +0900

    [init] sql-query-executor

--- src/main/java/pmg/study/Main.java
+++ src/main/java/pmg/study/Main.java
@@ -2,6 +2,9 @@ package pmg.study;
 
 import pmg.study.connection.DBConnection;
 
+/**
+ * made by pmg
+ */
 public class Main {
     public static void main (String[] args) {
         System.out.println("System LOG : Query Executor started...");
